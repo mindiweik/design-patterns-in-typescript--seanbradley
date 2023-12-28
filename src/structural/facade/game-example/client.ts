c44a7a770b086758b62a3a43f86e5505e5c860e8
Mindi Weik <mindi@sierrainteractive.com>
2023-12-28 15:26:32 -0700

    Facade Pattern use case example

---
+++ src/structural/facade/game-example/client.ts
@@ -0,0 +1,48 @@
+// The Facade Example Use Case
+
+import GameAPI from './game-api'
+
+function sleep(ms: number) {
+	return new Promise(resolve => setTimeout(resolve, ms))
+}
+
+async function facadeExample() {
+	const gameAPI = new GameAPI()
+
+	const user = { user_name: 'mindi' }
+	const userId = gameAPI.registerUser(user)
+
+	await sleep(500)
+
+	gameAPI.submitEntry(userId, 5)
+
+	await sleep(500)
+
+	console.log()
+	console.log('---- GameState Snapshot ----')
+	console.log(gameAPI.gameState())
+
+	await sleep(1000)
+
+	const HISTORY = gameAPI.getHistory(userId)
+
+	console.log()
+	console.log('---- Reports History ----')
+	Object.keys(HISTORY).forEach((key: string) => {
+		console.log(`${key}: ${HISTORY[key][0]} : ${HISTORY[key][1]}`)
+	})
+
+	await sleep(1000)
+
+	console.log()
+	console.log('---- User Balance ----')
+	console.log(user.user_name + ' : ' + gameAPI.getBalance(userId))
+
+	await sleep(1000)
+
+	console.log()
+	console.log("---- GameState Snapshot ----")
+	console.log(gameAPI.gameState())
+}
+
+facadeExample()
