17c0e6a019b9f4143ca53dd3741a45b7b34b745d
Mindi Weik <mindi@sierrainteractive.com>
2024-04-04 15:17:21 -0600

    use case examples added

---
+++ src/structural/proxy/future-example/iproteus.ts
@@ -0,0 +1,10 @@
+// The Proteus Interface
+export default interface IProteus {
+// A Greek mythological figure that can change shape
+	
+	// Proteus will change form rather than tell you the future
+	tellMeTheFuture(): void
+
+	// The form of Proteus is elusive like the sea
+	tellMeYourForm(): void
+}
